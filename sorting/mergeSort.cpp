b04d927f47f1df873538016af242b20a8f0ae525
ireishp <ipurohi2@asu.edu>
2023-06-11 00:23:50 -0700

    Update mergeSort.cpp

--- sorting/mergeSort.cpp
+++ sorting/mergeSort.cpp
@@ -8,8 +8,21 @@ Author: Ireish P.
 #include <iostream>
 using namespace std;
 
+int merge(vector<int> arr, int l, int r, int m)
+{
+    
+}
+
 int mergeSort(vector<int> arr, int l, int r)
 {
+    if(l < r)
+    {
+        int m = (l+r)/2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+        merge(arr, l, r, m);
+    }
     
 }
 
