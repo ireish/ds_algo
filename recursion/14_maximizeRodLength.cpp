3e9643960e296d7e278e283fa089e4ea76a51bed
Poppin220µg <ireish@hotmail.com>
2024-02-25 15:10:52 -0700

    Update and rename 24_maximizeRodLength.cpp to 14_maximizeRodLength.cpp

--- recursion/14_maximizeRodLength.cpp
+++ recursion/14_maximizeRodLength.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 
+
 // Given 3 lengths a, b, c: Maximize the number of pieces in which the rod can be cut;
 // Each cut length should be either [a, b, c];
 // Eg: n=23, a=11, b=9, c=12 => O/P: 2;
@@ -12,19 +13,18 @@ int maxRodPieces(int n, int a, int b, int c, int ct) {
     
     if(n < 0) return -1;
     if(n == 0) {
-        maxi = max(maxi, ct);
         return ct;
     }
     
-    int countA = maxRodPieces(n-a, a, b, c, ct+1); 
-    int countB = maxRodPieces(n-b, a, b, c, ct+1);
-    int countC = maxRodPieces(n-c, a, b, c, ct+1);
+    // int countA = maxRodPieces(n-a, a, b, c, ct+1); 
+    // int countB = maxRodPieces(n-b, a, b, c, ct+1);
+    // int countC = maxRodPieces(n-c, a, b, c, ct+1);
     
-    if(countA==-1 && countB==-1 && countC==-1) {
-        return -1;
-    }
+    // if(countA==-1 && countB==-1 && countC==-1) {
+    //     return -1;
+    // }
     
-    return max(countA, max(countB, countC));
+    return max(maxRodPieces(n-a, a, b, c, ct+1), max(maxRodPieces(n-b, a, b, c, ct+1), maxRodPieces(n-c, a, b, c, ct+1) ) );
 }
 
 int main() {
