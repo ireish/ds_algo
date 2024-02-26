322073d931dd876b4dd4af6db64473de9f8b83e6
Poppin220µg <ireish@hotmail.com>
2024-02-26 13:29:24 -0700

    Update 15_generateBalancedParantheses.cpp
    slightly better runtime;

--- recursion/15_generateBalancedParantheses.cpp
+++ recursion/15_generateBalancedParantheses.cpp
@@ -1,21 +1,19 @@
 void gP(vector<string>& ans, int opn, int cln, string out) {
   
-  if(opn == 0) {
-      while(cln!=0) {
-          out += ")";
-          --cln;
-      }
+  if(opn == 0 && cln==0) {
       ans.push_back(out);
       return;
   }
-  
-  if(cln < opn) return;
 
-  string out1 = out + "(";
-  gP(ans, opn-1, cln, out1);
+  if(opn != 0) {
+      string out1 = out + "(";
+      gP(ans, opn-1, cln, out1);
+  }
 
-  string out2 = out + ")";
-  gP(ans, opn, cln-1, out2);
+  if(cln > opn) {
+      string out2 = out + ")";
+      gP(ans, opn, cln-1, out2);
+  }
 }
 
 vector<string> generateParenthesis(int n) {
