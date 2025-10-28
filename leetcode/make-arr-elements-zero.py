33e416cdeaeb32bc9fb183f7efd44ca14e881c77
Elon Musk <ireish@hotmail.com>
2025-10-27 23:59:21 -0700

    add: lc-3554

---
+++ leetcode/make-arr-elements-zero.py
@@ -0,0 +1,86 @@
+"""
+3354. Make Array Elements Equal to Zero
+
+You are given an integer array nums.
+
+Start by selecting a starting position curr such that nums[curr] == 0, and choose a movement direction of either left or right.
+
+After that, you repeat the following process:
+
+If curr is out of the range [0, n - 1], this process ends.
+
+If nums[curr] == 0, move in the current direction by incrementing curr if you are moving right, or decrementing curr if you are moving left.
+Else if nums[curr] > 0:
+
+Decrement nums[curr] by 1.
+Reverse your movement direction (left becomes right and vice versa).
+Take a step in your new direction.
+
+A selection of the initial position curr and movement direction is considered valid if every element in nums becomes 0 by the end of the process.
+
+Return the number of possible valid selections.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,2,0,3]
+Output: 2
+
+Explanation:
+
+The only possible valid selections are the following:
+
+Choose curr = 3, and a movement direction to the left.
+[1,0,2,0,3] -> [1,0,2,0,3] -> [1,0,1,0,3] -> [1,0,1,0,3] -> [1,0,1,0,2] -> [1,0,1,0,2] -> [1,0,0,0,2] -> [1,0,0,0,2] -> [1,0,0,0,1] -> [1,0,0,0,1] -> [1,0,0,0,1] -> [1,0,0,0,1] -> [0,0,0,0,1] -> [0,0,0,0,1] -> [0,0,0,0,1] -> [0,0,0,0,1] -> [0,0,0,0,0].
+Choose curr = 3, and a movement direction to the right.
+[1,0,2,0,3] -> [1,0,2,0,3] -> [1,0,2,0,2] -> [1,0,2,0,2] -> [1,0,1,0,2] -> [1,0,1,0,2] -> [1,0,1,0,1] -> [1,0,1,0,1] -> [1,0,0,0,1] -> [1,0,0,0,1] -> [1,0,0,0,0] -> [1,0,0,0,0] -> [1,0,0,0,0] -> [1,0,0,0,0] -> [0,0,0,0,0].
+
+Example 2:
+
+Input: nums = [2,3,4,0,4,1,0]
+Output: 0
+
+Explanation:
+
+There are no possible valid selections.
+"""
+
+
+# Brute force by simulating the process for each possible initial position and movement direction
+def countValidSelections(self, nums: List[int]) -> int:
+
+    result = 0
+
+    def _simulate(arr: List[int], dir: int, curr: int):
+
+        while 0 <= curr < len(nums):
+
+            curr += dir
+            
+            if curr < 0 or curr >= len(arr):
+                return
+
+            if arr[curr] > 0:
+                arr[curr] -= 1
+                dir *= -1
+
+
+
+    for i, elem in enumerate(nums):
+
+        if elem == 0:
+
+            nums_temp = copy.deepcopy(nums) 
+            
+            _simulate(nums_temp, -1, i)
+            if all(x == 0 for x in nums_temp):
+                result += 1
+
+            nums_temp = copy.deepcopy(nums)
+
+            _simulate(nums_temp, 1, i)
+            if all(x == 0 for x in nums_temp):
+                result += 1
+    
+    return result
